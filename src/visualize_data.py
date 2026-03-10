0a2a1765a057d212e68f23f55fe8d13a8101ec7b
Roy <royyi2006@gmail.com>
2026-03-10 15:22:06 -0700

    visualized percentage change

--- src/visualize_data.py
+++ src/visualize_data.py
@@ -9,7 +9,6 @@ FIGURES_DIR = PROJECT_ROOT / "figures"
 FIGURES_DIR.mkdir(parents=True, exist_ok=True)
 
 
-df = pd.read_csv(PROCESSED_DATA_DIR / "oil_futures/oil_futures_daily_with_volgk.csv", parse_dates=['Date'])
 
 def plot_volatility(df):
     plt.figure()
@@ -21,6 +20,25 @@ def plot_volatility(df):
     plt.savefig(FIGURES_DIR / "volatility_GK.png")
     plt.close()
 
+def plot_change(df):
+    plt.figure()
+    plt.plot(df['Date'], df['decimal_change'], linestyle='-', color='red')
+    plt.plot(df['Date'], df['lag_1_day'], linestyle='-', color='blue')
+    plt.plot(df['Date'], df['lag_3_day'], linestyle='-', color='green')
+    plt.plot(df['Date'], df['lag_5_day'], linestyle='-', color='yellow')
+    plt.title("daily bdti")
+    plt.xlabel("Dates")
+    plt.ylabel("Percentage change")
+    plt.grid()
+    plt.savefig(FIGURES_DIR / "bdti.png")
+    plt.close()
+
+
+oil_df = pd.read_csv(PROCESSED_DATA_DIR / "oil_futures/oil_futures_daily_with_volgk.csv", parse_dates=['Date'])
+bdti_df = pd.read_csv(PROCESSED_DATA_DIR / "bdti/bdti_with_change_and_lag.csv", parse_dates=['Date'])
+
+
 if __name__ == "__main__":
-    plot_volatility(df)
+    plot_volatility(oil_df)
+    plot_change(bdti_df)
 
