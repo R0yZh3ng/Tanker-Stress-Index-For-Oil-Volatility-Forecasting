a104bafc25fa3a2629de413afe4232703e79a51a
Roy <royyi2006@gmail.com>
2026-03-10 14:37:28 -0700

    added Baltic Dirty Tanker (daily freight rates for transporting crude oil and heavy fuel oil on 17 major routes), attempted to added scraping from investing.com but failed, so added csv manually.

--- src/download_data.py
+++ src/download_data.py
@@ -1,4 +1,6 @@
 from utils.paths import RAW_DATA_DIR
+import requests
+from datetime import datetime
 
 import yfinance as yf
 import pandas as pd
@@ -9,15 +11,39 @@ RAW_OIL_FUTURES_DIR.mkdir(parents=True, exist_ok=True)
 
 def download_oil_futures(start, end, interval):
     #NOTE: df stands for dataframe since yfinance download saves historical_data as a pandas dataframe
-    df = yf.download("CL=F", start=start, end=end, interval=interval, multi_level_index=False) #multi level index set to false to only get the indexes i need
-    df.to_csv(RAW_OIL_FUTURES_DIR / "oil_futures_daily.csv")
+    oil_df = yf.download("CL=F", start=start, end=end, interval=interval, multi_level_index=False) #multi level index set to false to only get the indexes i need
+    oil_df.to_csv(RAW_OIL_FUTURES_DIR / "oil_futures_daily.csv")
 
-    print(df.head())
+    print(f"Saved oil futures data with {len(oil_df)} rows")
 
 
+RAW_BDTI_DIR = RAW_DATA_DIR / "bdti"
+RAW_BDTI_DIR.mkdir(parents=True, exist_ok=True)
 
+#NOTE:below is the failed attempt at scraping investing.com, unfortunately have to manually import the data for now 
+
+#def download_BDTI(start, end):
+#    url = "https://www.investing.com/indices/baltic-dirty-tanker-historical-data"
+#    
+#    headers = {
+#        "User-Agent": "Mozilla/5.0"
+#    }
+#    
+#    tables = pd.read_html(requests.get(url, headers=headers).text)
+#    bdti_df = tables[0]
+#    
+#    bdti_df["Date"] = pd.to_datetime(bdti_df["Date"])
+#    bdti_df = bdti_df[["Date", "Price"]].rename(columns={"Price": "BDTI"})
+#    bdti_df = bdti_df.sort_values("Date")
+#    
+#    bdti_df.to_csv("data/raw/bdti/bdti_daily.csv", index=False)
+#    print(f"Saved BDTI data with {len(bdti_df)} rows")
 
 #NOTE: this idiom checks if a script is being run directly as the main program or being imported as a module into another script.
 #      we make sure that the specified functions only run if its being ran directly from this program
 if __name__ == "__main__":
-    download_oil_futures("2020-01-01", "2026-01-01", "1d")
+    start = pd.to_datetime("2020-01-01")
+    end = pd.to_datetime("2026-01-01")
+
+    download_oil_futures(start, end, "1d")
+    #download_BDTI(start, end)
--- src/process_data.py
+++ src/process_data.py
@@ -7,6 +7,12 @@ from utils.paths import RAW_DATA_DIR, PROCESSED_DATA_DIR
 PROCESSED_OIL_FUTURES_DIR = PROCESSED_DATA_DIR / "oil_futures"
 PROCESSED_OIL_FUTURES_DIR.mkdir(parents=True, exist_ok=True)
 
+PROCESSED_BDTI_DIR = PROCESSED_DATA_DIR / "bdti"
+PROCESSED_BDTI_DIR.mkdir(parents=True, exist_ok=True)
+
+PROCESSED_FINAL = PROCESSED_DATA_DIR / "final_concoction"
+PROCESSED_FINAL.mkdir(parents=True, exist_ok=True)
+
 def garman_klass_volatility(df, window):
     """
     Rolling Garman Klass volatility estimator
@@ -24,12 +30,51 @@ def garman_klass_volatility(df, window):
     return rolling_vol
 
 
-df = pd.read_csv(RAW_DATA_DIR / "oil_futures/oil_futures_daily.csv", parse_dates=["Date"])
-df.set_index('Date', inplace=True) #allows dates to be read as a date time object and used as a index
+oil_df = pd.read_csv(RAW_DATA_DIR / "oil_futures/oil_futures_daily.csv", parse_dates=["Date"])
+oil_df.set_index('Date', inplace=True) #allows dates to be read as a date time object and used as a index
 
 #NOTE: this adds a column using the function
-df['vol_gk'] = garman_klass_volatility(df, 20)
+oil_df['vol_gk'] = garman_klass_volatility(oil_df, 20)
 
-df.to_csv(PROCESSED_OIL_FUTURES_DIR / "oil_futures_daily_with_volgk.csv")
+oil_df.to_csv(PROCESSED_OIL_FUTURES_DIR / "oil_futures_daily_with_volgk.csv")
 
-print(df.tail())
+print(f"added vol_gk to oil_futures data {len(oil_df) - 20} rows of data (excluding the first 20 since we are using a rolling window of 20)")
+
+
+#NOTE: the reason why the below is commented out is that it works but investing.com provided bad data, will have to work with what i have for now, so commenting this out to save resources
+
+#def bdti_daily_range(df):
+#    """
+#    Daily percentage range calculation for bdti data
+#    """
+#    #NOTE: higher bdti range tells us that the tanker market stress spiked, using this to correlate to futures price volatility is more accurate
+#
+#    #NOTE: the blow code changes the columns to numbers instead and remove commas if neccesary
+#
+#    df["Price"] = pd.to_numeric(df["Price"].str.replace(",", ""), errors="coerce")
+#    df["High"] = pd.to_numeric(df["High"].str.replace(",", ""), errors="coerce")
+#    df["Low"] = pd.to_numeric(df["Low"].str.replace(",", ""), errors="coerce")
+#    df["Open"] = pd.to_numeric(df["Open"].str.replace(",", ""), errors="coerce")
+#    df["Change %"] = pd.to_numeric(df["Change %"].str.replace(",", ""), errors="coerce")
+#
+#
+#    return (df['High'] - df['Low'])/df['Open']
+#
+#bdti_df = pd.read_csv(RAW_DATA_DIR / "bdti/Baltic_Dirty_Tanker_daily.csv", parse_dates=["Date"])
+#bdti_df.set_index('Date', inplace=True)
+#
+#bdti_df['Range'] = bdti_daily_range(bdti_df)
+
+bdti_df = pd.read_csv(RAW_DATA_DIR / "bdti/Baltic_Dirty_Tanker_daily.csv", parse_dates=["Date"])
+bdti_df.set_index('Date', inplace=True)
+
+bdti_df["decimal_change"] = (bdti_df["Change %"].str.strip("%").str.replace(",", "").astype(float))
+
+#NOTE: create lagged days because market reaction to data is often delayed
+bdti_df["lag_1_day"] = bdti_df["decimal_change"].shift(1)
+bdti_df["lag_3_day"] = bdti_df["decimal_change"].shift(3)
+bdti_df["lag_5_day"] = bdti_df["decimal_change"].shift(5)
+
+bdti_df.to_csv(PROCESSED_BDTI_DIR / "bdti_with_daily_range.csv")
+
+print(f"added daily percentage to bdti data of {len(bdti_df)} rows")
